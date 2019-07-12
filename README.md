7b6498bd8066474c74652e3d6e462bf5e35ac526
Subhajit Chatterjee <subhajitchatterjee425@gmail.com>
2019-07-13 01:11:46 +0530

    set up the svg along with changing temperature and added click to change the temperature

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # weather-webapp
-simple web-app which can show weather information using geo-location and other weather api.
+simple weather app which can show weather information using geo-location and other weather api.
--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 window.addEventListener('load', ()=>{
     let long;
     let lat;
+    let temperatureDesription = document.querySelector(".temperature-desription");
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let locationTimezone = document.querySelector(".location-timezone");
+    let temperatureSection = document.querySelector(".temperature");
+    const temperatureSpan = document.querySelector(".temperature span");
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
@@ -8,41 +13,53 @@ window.addEventListener('load', ()=>{
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
-            //const api = `api.openweathermap.org/data/2.5/weather?${lat},${long}`;
 
-            /*fetch(api)
-                .then(data =>{
-                    return data.json()//return data.json()//we are taking that information and changing it to json so with json we can easily use it to JS
-                    //console.log(JSON.stringify(data));
+            const proxy = "http://cors-anywhere.herokuapp.com/";
+            //it will give a cors issue** so we'll be using a proxy
+            const api = `${proxy}https://api.darksky.net/forecast/68568119ac38ef741a51540efb8ad8b3/${lat},${long}`;
+            fetch(api)
+            .then(data => {
+              return data.json();
+            })
+            .then(data => {
+                console.log(data);
+                
+                //data.currently.temperatue
+                const {temperature, summary, icon} = data.currently;//es2015 shorthand
+
+                //set DOM elements form the api
+                temperatureDegree.textContent = temperature;
+                temperatureDesription.textContent = summary;
+                locationTimezone.textContent =  data.timezone;
+
+                //FORMULA FOR CELSIUS
+                let celsius = (temperature -32) * (5/9);
+
+                //SET ICON
+                setIcons(icon, document.querySelector(".icon"));
+
+                //Change temperature form f to c
+                temperatureSection.addEventListener("click", () => {
+                    if(temperatureSpan.textContent === "F"){
+                        temperatureSpan.textContent = "C";
+                        temperatureDegree.textContent = Math.floor(celsius);
+                    }else{
+                        temperatureSpan.textContent = "F";
+                        temperatureDegree.textContent = temperature;
+                    }
                 });
-                .then(data =>{
-                    console.log(data);
-                });*/
-
-                /*fetch('https://api.darksky.net/forecast/68568119ac38ef741a51540efb8ad8b3/$(long),$(lat)id=68568119ac38ef741a51540efb8ad8b3');
-                 .then(response => response.json())
-                 .then(json => console.log(json))*/
-
-                  const proxy = "http://cors-anywhere.herokuapp.com/";
-
-                  const api = `${proxy}https://api.darksky.net/forecast/68568119ac38ef741a51540efb8ad8b3/${lat},${long}`;
-                  fetch(api)
-                    .then(data => {
-                      return data.json();
-                  })
-                    .then(data => {
-                    console.log(data);
-                    });
-                    //it will give a cors issue** so we'll be using a proxy
-
-
-
-
+            });
         });
+    }else{
+        h1.textContext = "hey this is not working because of some mystical reasons XD"
+    }
 
-    }/*else{
-        h1.textContext = "hey this is not working because of reasons"
-    }*/
 
-
+    //SETTING UP ICON FORM SKYCONS
+    function setIcons(icon, iconId){
+        const skycons = new Skycons({"color": "white"});
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconId, Skycons[currentIcon]);
+    }
 });
